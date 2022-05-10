a054255ed29b927e260c1cf953369051ce9281e7
koujyungenn <513137767@qq.com>
2022-05-10 21:02:18 +0800

    ES文档的创建和普通查询(part1)

---
+++ src/main/java/com/genn/es/es/HotelEsDTO.java
@@ -0,0 +1,124 @@
+package com.genn.es.es;
+
+import com.genn.es.entity.Hotel;
+
+public class HotelEsDTO {
+    private HotelEsDTO(){}
+
+    private Long id;
+    private String name;
+    private String address;
+    private String price;
+    private String score;
+    private String brand;
+    private String city;
+    private String starName;
+    private String business;
+    private String location;
+    private String pic;
+
+
+    public static HotelEsDTO getEsDto(Hotel hotel){
+        HotelEsDTO esDTO = new HotelEsDTO();
+        esDTO.setName(hotel.getName());
+        esDTO.setAddress(hotel.getAddress());
+        esDTO.setPrice(hotel.getPrice());
+        esDTO.setScore(hotel.getScore());
+        esDTO.setId(hotel.getId());
+        esDTO.setBrand(hotel.getBrand());
+        esDTO.setCity(hotel.getCity());
+        esDTO.setStarName(hotel.getStarName());
+        esDTO.setBusiness(hotel.getBusiness());
+        esDTO.setLocation(String.format("%s,%s",hotel.getLatitude(), hotel.getLongitude()));
+        esDTO.setId(hotel.getId());
+        return esDTO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStarName() {
+        return starName;
+    }
+
+    public void setStarName(String starName) {
+        this.starName = starName;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+}
--- src/test/java/com/genn/es/EsRelearnTest.java
+++ src/test/java/com/genn/es/EsRelearnTest.java
@@ -1,7 +1,15 @@
 package com.genn.es;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.genn.es.Repository.HotelMapper;
 import com.genn.es.entity.Hotel;
+import com.genn.es.es.HotelEsDTO;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
@@ -11,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 public class EsRelearnTest {
@@ -28,11 +37,48 @@ public class EsRelearnTest {
 
     String createHotelDSL = "{\"mappings\":{\"properties\":{\"id\":{\"type\":\"keyword\"},\"name\":{\"type\":\"text\",\"analyzer\":\"ik_max_word\",\"copy_to\":\"brandAndName\"},\"address\":{\"type\":\"text\",\"analyzer\":\"ik_max_word\"},\"price\":{\"type\":\"double\"},\"score\":{\"type\":\"double\"},\"brand\":{\"type\":\"keyword\",\"copy_to\":\"brandAndName\"},\"city\":{\"type\":\"keyword\"},\"starName\":{\"type\":\"keyword\"},\"business\":{\"type\":\"text\"},\"location\":{\"type\":\"geo_point\"},\"brandAndName\":{\"type\":\"text\",\"analyzer\":\"ik_max_word\"}}}}";
 
+    /**
+     * 新建索引
+     * @throws IOException
+     */
     @Test
     public void testCreateIndex() throws IOException {
         String indexName = "hotel";
         CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
         createIndexRequest.source(createHotelDSL, XContentType.JSON);
         restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 新建文档
+     * @throws IOException
+     */
+    @Test
+    public void testCreateDocument() throws IOException {
+        Hotel hotel = hotelMapper.selectById(38665L);
+        HotelEsDTO esDto = HotelEsDTO.getEsDto(hotel);
+        String indexName = "hotel";
+
+        IndexRequest request = new IndexRequest(indexName).id(String.valueOf(esDto.getId()));
+        request.source(JSONObject.toJSONString(esDto), XContentType.JSON);
+        IndexResponse index = restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        System.out.println(JSONObject.toJSONString(index));
+    }
+
+    /**
+     * 批量插入数据库的hotel到ES
+     */
+    @Test
+    public void testBulkInsert() throws IOException {
+        List<Hotel> hotels = hotelMapper.selectList(null);
+        BulkRequest bulkRequest = new BulkRequest();
+        for (Hotel hotel : hotels) {
+            HotelEsDTO esDto = HotelEsDTO.getEsDto(hotel);
+            bulkRequest.add(new IndexRequest("hotel")
+                    .id(String.valueOf(esDto.getId()))
+                    .source(JSONObject.toJSONString(esDto),XContentType.JSON));
+        }
+        BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(JSONObject.toJSONString(bulk));
     }
 }
